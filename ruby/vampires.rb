df6e4596ea9adf4f65ddc73804a9752a4f1af354
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-02-17 17:49:29 -0600

    Add and/or statements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,7 +3,7 @@ puts "What is your name?"
 name = gets.chomp
 
 puts " How old are you?"
-age = gets.chomp
+age = Integer(gets.chomp)
 
 puts "When were you born?"
 birth = gets.chomp
@@ -13,4 +13,19 @@ garlic = gets.chomp
 
 puts " Do you plan on enrolling in the company's health insurance?"
 insurance = gets.chomp
+
+
+wolves_like_sunshine = true
+wolves_like_garlic = true
+vampires_like_sunshine = false
+vampires_like_garlic = false
+
+p wolves_like_garlic && wolves_like_garlic
+p wolves_like_garlic || vampires_like_garlic
+p wolves_like_garlic && vampires_like_garlic
+p wolves_like_garlic && (vampires_like_sunshine || vampires_like_garlic)
+p (wolves_like_garlic && vampires_like_garlic) || wolves_like_sunshine
+p vampires_like_garlic
+p !vampires_like_garlic
+p !(wolves_like_sunshine && wolves_like_garlic)
 
