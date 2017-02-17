1f01c42396d81742e3484689dd44b498c9fecf11
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-02-17 14:07:41 -0600

    Add employee questioniarre to ruby file

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+#Employee Questionnaire
+puts "What is your name?"
+name = gets.chomp
+
+puts " How old are you?"
+age = gets.chomp
+
+puts "When were you born?"
+birth = gets.chomp
+
+puts " Would you like us to order some garlic for you?"
+garlic = gets.chomp
+
+puts " Do you plan on enrolling in the company's health insurance?"
+insurance = gets.chomp
+
