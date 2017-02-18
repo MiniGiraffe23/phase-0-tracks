d5584c6857961b56fd2bda2c37a43beb1a57fe28
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-02-17 23:20:50 -0600

    fix errors in code to ensure encrypt and decrypt methogs ran properly

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,24 +1,63 @@
-def encrypt(string)
-	string.tr!("z", "!")
+def encrypt(password)
+#create loop advancing each character of a string one letter forward
+#declare variable for use in loop
 	x = 0
-	until x == string.length
-		string[x] = string[x].next
-		x += 1
+#initiate a loop that incremntally changes the x up to the 
+#length of the password, ensuring process is run on each character
+
+	while x < password.length	
+		password[x] = password[x].next
+	if password[x] == "!"
+		password[x] = " "
+	elsif password[x] == "z"
+			password[x] = "a"
 	end
 
-	string.tr!('!', ' ') 
-	string.tr!('"', 'a')
+	encryption = password
+	x += 1
+	end
+   return encryption
 end
-	 
-def decrypt(string)
-	string.tr!(' ', '!')
-	string.tr!('a', ' ')
-	string2 = "abcdefghijklmnopqrstuvwxyz !"
 
 
-	y = 0
-	until y == string.length
-		string[y] = string2[string2.index(string[y]) - 1]
+def decrypt(encryption)
+#declare a variable for the alphabet to use as a point of reference for 
+#decryption 
+ reference = "abcdefghijklmnopqrstuvwxyz"
+ y = 0
+
+	until y == encryption.length
+		encryption[y] = reference[reference.index(encryption[y]) - 1]
+	if encryption[y] == " "
+		encryption[y] = "!"
+	elsif encryption[y] =="'"
+			encryption[y] = " "
+		end
+
+	decryption = encryption
 		y += 1
-	end
+	end 
+	return decryption
+
 end
+
+puts "Would you like to encrypt or decrpyt your password?"
+answer = gets.chomp
+puts " Please enter your password."
+user_password = gets.chomp
+	
+	if answer == "encrypt"
+			puts encrypt(user_password)
+	elsif answer == "decrypt"
+			puts decrypt(user_password)
+	else
+		puts "Please try again."
+	end
+
+=begin
+puts encrypt("abc")
+puts encrypt("zed")
+puts decrypt("bcd")
+puts decrypt("afe")
+puts decrypt(encrypt("swordfish"))
+=end
