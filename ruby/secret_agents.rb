7cc7053495e422d51febd8116d93d3c9f4d508f0
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-02-18 00:04:26 -0600

    fix typos and code error where z was encrypting to ab instead of a

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,12 +1,17 @@
 def encrypt(password)
+
 #create loop advancing each character of a string one letter forward
 #declare variable for use in loop
+
 	x = 0
+
 #initiate a loop that incremntally changes the x up to the 
 #length of the password, ensuring process is run on each character
 
-	while x < password.length	
+	while x < password.length
+		unless password[x] == "z"	
 		password[x] = password[x].next
+	end
 	if password[x] == "!"
 		password[x] = " "
 	elsif password[x] == "z"
@@ -19,25 +24,21 @@ def encrypt(password)
    return encryption
 end
 
-
 def decrypt(encryption)
 #declare a variable for the alphabet to use as a point of reference for 
 #decryption 
+
  reference = "abcdefghijklmnopqrstuvwxyz"
 
- #define variable 
+ #define variable
+
  y = 0
 
 #initiate loop to decrypt password by using reference to reverse each character by one character
+	
 	until y == encryption.length
 		encryption[y] = reference[reference.index(encryption[y]) - 1]
 		# below attempt at decrypting spaces...did not work
-
- y = 0
-
-	until y == encryption.length
-		encryption[y] = reference[reference.index(encryption[y]) - 1]
-
 	if encryption[y] == " "
 		encryption[y] = "!"
 	elsif encryption[y] =="'"
@@ -51,23 +52,18 @@ def decrypt(encryption)
 
 end
 
-
 #prompt user to decide what he/she wants to do with their password
+
 puts "Would you like to encrypt or decrpyt your password?"
 answer = gets.chomp
 
 #prompt user to enter a password
+
 puts " Please enter your password."
 user_password = gets.chomp
 
 #instruct to encrypt or decrypt user's password based on their choice of either 
-#encrypt or decrypt	
-
-puts "Would you like to encrypt or decrpyt your password?"
-answer = gets.chomp
-puts " Please enter your password."
-user_password = gets.chomp
-	
+#encrypt or decrypt    
 
 	if answer == "encrypt"
 			puts encrypt(user_password)
@@ -77,14 +73,11 @@ user_password = gets.chomp
 		puts "Please try again."
 	end
 
-
 =begin 
-=======
-=begin
->>>>>>> 28ea7c502854739c5bedad7d5ea6625beeb8445a
 puts encrypt("abc")
 puts encrypt("zed")
 puts decrypt("bcd")
 puts decrypt("afe")
 puts decrypt(encrypt("swordfish"))
 =end
+
