c0732ea3d272b0dc86a6e761e0994104142c4798
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-02-24 18:17:43 -0600

    Begin work on 3.2 assignments by assigning symbols in values for hash

---
+++ ruby/hashes.rb
@@ -0,0 +1,47 @@
+=begin
+pseudocode and write a program that will allow an interior designer to enter the details of a given client: the client's name, age, number of children, decor theme, and so on (you can choose your own as long as it's a good mix of string, integer, and boolean data).
+Your keys should be symbols unless you find that you need a string for some reason -- usually only when spaces or other "user friendly" formatting are needed (as in the case of using someone's full name as a hash key). Basically, symbols are simpler for programmers to use, and readable enough by technical folks to be used in place of strings most of the time.
+Your program should ...
+- Prompt the designer/user for all of this information.
+- Convert any user input to the appropriate data type.
+- Print the hash back out to the screen when the designer has answered all of the questions.
+- Give the user the opportunity to update a key (no need to loop, once is fine). After all, sometimes users make mistakes! If the designer says "none", skip it. But if the designer enters "decor_theme" (for example), your program should ask for a new value and update the :decor_theme key. (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
+Print the latest version of the hash, and exit the program.
+Be sure to pseudocode, and leave your pseudocode in as comments.
+=end
+
+client_design_preference = {
+
+	name: " ",
+	address: " ",
+	age: " ",
+	number_of_children: " ",
+	spend_amount: " ",
+	decor_theme: " ",
+	prefered_stores: " ",
+	favorite_colors: " ",
+	paint_preference: " ",
+
+}
+
+puts "Welcome to your client design organizer."
+puts "Here you can store all your data about various clients, and I will keep it nice and organized for you."
+puts "Please answer the following questions about your client."
+
+
+puts "What is your client's full name?"
+client_design_preference[:name] = gets.chomp.capitalize
+
+puts "Please double check the name you entered. If \"#{client_design_preference[:name]}\" is correct, type \"yes\", if the name is incorrect, type \"no\"."
+correct_name = gets.chomp
+	if correct_name == "yes"
+		client_design_preference[:name]
+	elsif correct_name	== "no"
+		puts " Please re-enter client's name."
+		client_design_preference[:name] = gets.chomp.capitalize
+	else
+		puts "I do not understand. Please try again."
+	end 
+
+puts "What is the address of \"#{client_design_preference[:name]}\"?"
+
