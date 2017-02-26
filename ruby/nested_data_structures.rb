5c0e0aa549e09824e03216de1b9a8faa9ecf3167
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-02-25 19:04:03 -0600

    Pseudocode nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,21 @@
+#3.4 Assignment
+=begin
+-Build a nested data structure to represent a real-world construct. 
+-You can choose the construct: 
+--Is it a highway full of cars full of people? 
+--A classroom full of desks full of supplies? 
+--A fashion show with multiple runways featuring multiple models? 
+-Build something that will use a mix of hashes and arrays.
+-Print a few individual pieces of deeply nested data
+-Use multiple indexes and hash keys to access the nested items
+-Demonstrate a few different types of access.
+=end
+
+#Exhibits in a Zoo
+#What will we need:
+#Zoo name, different exhibit names within the zoo, animals/attractions within those exhibits
+
+Birdybuns_Zoo = {
+
+	Fascinating_Reptiles: 	}
+}
