6f4e9eb81993169c38df3c11ed5b7df19474d20e
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-03-01 13:48:18 -0600

    Create puppy class by listing various characteristics and behaviors

---
+++ ruby/puppy.rb
@@ -0,0 +1,31 @@
+#PUPPY CLASS
+#Include characteristics and behaviors
+
+# - SPECIES -
+# - Pupulous Maximus -
+
+# - CHARACTERISTICS -
+
+# Age: 
+# Fur Color:
+# Breed:
+# Gender:
+# Name:
+# Hypoallergenic:
+# Hair type:
+# Weight:
+
+
+# - BEHAVIORS -
+ 
+# Barks
+# Fetches
+# Bites
+# Kisses
+# Licks
+# Run
+# Walk
+# Cuddles
+# Whines
+
+
