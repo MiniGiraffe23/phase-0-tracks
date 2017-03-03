345f5ccc7c9cd2f6e91847d90bb29b99b2efb83e
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-03-02 19:52:32 -0600

    Add pseudocode, define class and add speak and eats milk and cookies methods

---
+++ ruby/santa.rb
@@ -0,0 +1,17 @@
+#Build a Santa class
+#Include following methods:
+# --- Speak: prints "Ho, ho, ho! Haaaappy holidays!"
+#---- Eat milk and cookies: takes cookie type as parameter and prints "That was a good #cookie type!"
+#---- Initialize: prints "Initializing Santa instance..."
+#Add code below class declaration to check initialize instance and methods work
+
+class Santa
+
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie_type)
+		puts "That was a good #{cookie_type}!"
+	end
+
