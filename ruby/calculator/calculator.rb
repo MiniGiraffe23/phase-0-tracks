ab074fd42df11712ff91ffdd55f884eafac80229
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-03-03 14:41:26 -0600

    Practice with calculator methods and rspec

--- ruby/calculator/calculator.rb
+++ ruby/calculator/calculator.rb
@@ -4,18 +4,18 @@
 
 class Calculator
   def add(x,y)
-    x + y
+  p  x + y
   end
 
   def subtract(x,y)
-    x - y
+  p  x - y
   end
 
   def multiply(x,y)
-    x * y
+  p  x * y
   end
 
   def divide(x,y)
-  	x / y
+  p	 x / y
   end
 end
