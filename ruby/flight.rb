90412dd86bec362bb4b8a5f3e0dd52b70b19a9fb
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-03-03 20:00:56 -0600

    Declare flight module

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -1,8 +1,15 @@
 #6.4: Release 2
 #Declare classes for a bird and a plane
 
+#Declare a Flight module (needs to go before declared classes)
+
+module Flight
+end
+
 class Bird
 end
 
 class Plane
 end
+
+ 
