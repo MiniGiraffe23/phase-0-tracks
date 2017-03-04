2e350e37386e720dd53da61e7e1bdeae3ab50a92
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-03-03 19:43:59 -0600

    Add some information regarding self keyword and add yelling happily method to shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -3,7 +3,7 @@
 #Declare shout method
 module Shout
 	#define method for yelling angrily
-	def self.yell_angrily(words)
+	def self.yell_angrily(words) #'self' keyword needed when defining methods of standalone modules
 		words + "!!!" + " :( "
 	end
 end
