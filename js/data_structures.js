ec355a41a8d53668b8633cab15e43350e6160e48
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-03-09 18:40:03 -0600

    Complete Release one

--- js/data_structures.js
+++ js/data_structures.js
@@ -2,9 +2,18 @@ var colors = ["blue", "red", "yellow", "green"];
 
 var horseName = ["Mr. Ed", "Joey", "Sadie", "Sea Biscuit"];
 
-colors.push("purle");
+colors.push("purple");
 horseName.push("Buster");
 
-console.log(colors);
-console.log(horseName);
+//console.log(colors);
+//console.log(horseName);
+
+//RELEASE ONE
+
+var horses = {};
+for (var i = 0; i < horseName.length; i++) {
+	horses[horseName[i]] = colors[i];
+}
+
+console.log(horses);
 
