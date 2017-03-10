e6734e2077ae21714409b7845894ddbb80f8d3bc
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-03-09 18:58:55 -0600

    Construct car constructor function

--- js/data_structures.js
+++ js/data_structures.js
@@ -17,3 +17,15 @@ for (var i = 0; i < horseName.length; i++) {
 
 console.log(horses);
 
+//RELEASE TWO - constructor function for a car
+
+function Car(make, model, year) {
+	console.log("Our new car:", this);
+		this.make = make;
+		this.model = model;
+		this.year = year;
+		this.start = function() {console.log("Vroom!"); };
+
+	console.log("CAR INITIALIZATION COMPLETE");
+}
+
