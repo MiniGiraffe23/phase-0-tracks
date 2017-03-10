059187176e96ce21db0cd567e8e52f3454d85929
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-03-09 18:15:58 -0600

    Complete release 0 for 7.2)

---
+++ js/data_structures.js
@@ -0,0 +1,10 @@
+var colors = ["blue", "red", "yellow", "green"];
+
+var horseName = ["Mr. Ed", "Joey", "Sadie", "Sea Biscuit"];
+
+colors.push("purle");
+horseName.push("Buster");
+
+console.log(colors);
+console.log(horseName);
+
