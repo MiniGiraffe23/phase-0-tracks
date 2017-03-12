6a7c5a7cc6a0f71032067b09b90ccc323530f8f4
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-03-11 20:12:20 -0600

    Store result of function in a vailable. Right if statment that will print the variable if the condition is true

--- js/explore.js
+++ js/explore.js
@@ -1,6 +1,6 @@
 // 7.1 Assignment 
 
-// fuction to reverse a string
+// fuction to reverse a string (takes string as a parameter and spits our the reverse)
 //1. create an empty string to hold new  
 //2. Use for loop to to iterate backward through a string and store the result in a new variable
 //3. Have the function return the new string 
@@ -14,4 +14,17 @@ function reverseString(string) {
 	return reversedString;
 }
 
-console.log(reverseString('pizza'))
+//console.log(reverseString('pizza'));
+
+
+//console.log(unicorn) test that rversed string was stored in a new variable
+var string = "unicorn";
+var newUnicorn = reverseString(string);
+
+if  (string.length < 9) {
+	console.log(newUnicorn);
+} else {
+	console.log("It is a sad day without unicorns!");
+}
+
+
