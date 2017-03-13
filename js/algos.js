08eab93fc846fb42b6b1188d6e7ae3269fbc14e5
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-03-13 07:42:16 -0500

    Fix typo on line 13, beging constructing compare objects function

--- js/algos.js
+++ js/algos.js
@@ -10,7 +10,7 @@ var wordArray = ["apple", "pear", "butterfly", "bananas"];
 function longestWord(worrdArray) {
 	var longest = 0;
 	var word = "";
-	for (i = 0; i <= wordArray.length - 1; i++) {
+	for (i = 0; i < wordArray.length - 1; i++) {
 		longeset = wordArray[i].length;
 		if (wordArray[i].length > longest); {
 			word = wordArray[i];
@@ -21,4 +21,16 @@ function longestWord(worrdArray) {
 
 longestWord(wordArray);
 
-//returning last word not longet word
+//returning last word not longet word - fixed. Had line 13 set to <= instead of just <
+
+//build a function that takes two objects and checks to see if a key or value pair matches
+
+var personOne = {name: "Steven", age :54};
+var personTwo = {name: "Tamir", age: 54};
+
+function compareObjects(objectOne, objectTwo) {
+	
+	}
+
+compare(personOne, personTwo);
+//can't get this function to work either
