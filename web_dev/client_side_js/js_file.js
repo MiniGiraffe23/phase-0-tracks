baea01a2b3f74a32a55182c9058c6aed7c543c26
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-03-21 15:13:51 -0500

    Add js to hide text, insert button that reveals hidden text when clicked

--- web_dev/client_side_js/js_file.js
+++ web_dev/client_side_js/js_file.js
@@ -10,4 +10,28 @@ var links = document.getElementsByTagName('a');
 
 for (var i = 0; i < links.length; i++) {
 	links[i].style.color = "white";
+	};
+
+var newParagraph = document.createElement('p');
+var paragraphText = document.createTextNode("This is a new paragraph!");
+newParagraph.appendChild(paragraphText);
+
+var firstHeading = document.getElementById('firstHeading');
+firstHeading.appendChild(newParagraph);
+
+var headings = document.getElementsByClassName('hide_me');
+
+for (var i = 0; i < headings.length; i++) {
+	//headings[i].hidden = true;
+	headings[i].style.display = "none";
+};
+
+function displayText() {
+  for (var i = 0; i < headings.length; i++) {
+	headings[i].style.display = "block"; 
 	}
+};
+
+var button = document.getElementById('button');
+button.addEventListener("click", displayText);
+
