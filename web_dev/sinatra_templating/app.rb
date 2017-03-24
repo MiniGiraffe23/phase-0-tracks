f48b7c00f9941da8a4480d18f02e8f16c650349b
Brittany M. Baranowski <brittanybaranowski@gmail.com>
2017-03-24 18:41:57 -0500

    Create get and post method for remove student file

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -24,24 +24,17 @@ end
 # create new students via
 # a form
 post '/students' do
-  db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
-  redirect '/'
+  	db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
+ 	redirect '/'
 end
 
-get '/students/add_pet' do
- 	erb :add_pet
+get '/students/remove'
+	erb :remove_student
 end
 
-post '/students' do
-	db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
-	redirect '/'
+post '/students_remove' do
+	db.execute("DELETE FROM students WHERE (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
+	redirect '/students'
 end
-
-
-# post '/students_remove' do
-#	db.execute("DELETE FROM students WHERE (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
-#	db.execute("DELETE FROM students WHERE name=? ", [params['name']])
-#	redirect '/'
-#end
 # remove Kolby Mertz, 38, SF
 # add static resources
